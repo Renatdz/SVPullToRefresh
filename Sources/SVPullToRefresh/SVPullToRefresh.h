88067d25df81fc7dc9cc2a02c13f3e39ec0a5ffd
Renato Mendes <renatodz86@gmail.com>
2023-06-18 20:33:36 -0300

    spm project

---
+++ Sources/SVPullToRefresh/SVPullToRefresh.h
@@ -0,0 +1,23 @@
+//
+//  SVPullToRefresh.h
+//  SVPullToRefreshDemo
+//
+// Created by Sam Vermette on 23.04.12.
+// Copyright (c) 2012 samvermette.com. All rights reserved.
+//
+// https://github.com/samvermette/SVPullToRefresh
+//
+
+// this header file is provided for backwards compatibility and will be removed in the future
+// here's how you should import SVPullToRefresh now:
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SVPullToRefresh.
+FOUNDATION_EXPORT double SVPullToRefreshVersionNumber;
+
+//! Project version string for SVPullToRefresh.
+FOUNDATION_EXPORT const unsigned char SVPullToRefreshVersionString[];
+
+#import "UIScrollView+SVPullToRefresh.h"
+#import "UIScrollView+SVInfiniteScrolling.h"
